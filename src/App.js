ae3d38419a229b6add1ab3f648efe15cef88b9fa
YukDDong <ekfhd5537@naver.com>
2023-06-27 01:21:24 +0900

    feat: router 적용 및 간단한 Onboarding페이지와 Login페이지 추가
    router 적용 및 간단한 Onboarding페이지와 Login페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Onboarding from "./pages/Onboarding";
+import LoginPage from "./pages/LoginPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Onboarding />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,7 +1,11 @@
 function Onboarding() {
+  const moveToLogin = () => {
+    window.location.pathname = "/login";
+  };
   return (
     <>
       <div>Onboarding</div>
+      <button onClick={moveToLogin}>시작하기</button>
     </>
   );
 }
