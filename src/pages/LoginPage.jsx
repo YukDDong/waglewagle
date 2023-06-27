9e77a5a80c46b394b1c657d04cc38fd82b82617e
YukDDong <ekfhd5537@naver.com>
2023-06-28 00:58:21 +0900

    feat: login 페이지 css작업중
    login 페이지 css작업중

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -16,6 +16,32 @@ function LoginPage() {
     box-sizing: border-box;
   `;
 
+  const NavLogo = styled.h1`
+    width: 170px;
+    height: 40px;
+    background-color: rgba(139, 139, 139, 0.3);
+    font-size: 35px;
+    text-align: center;
+    line-height: 40px;
+    color: white;
+  `;
+
+  const Main = styled.main`
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+  `;
+
+  const MainDiv = styled.div`
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 100px;
+    box-sizing: border-box;
+  `;
+
   const Logo = styled.h1`
     width: 170px;
     height: 40px;
@@ -23,14 +49,7 @@ function LoginPage() {
     font-size: 35px;
     text-align: center;
     line-height: 40px;
-  `;
-
-  const Main = styled.main`
-    width: 100%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    margin-bottom: 30px;
   `;
 
   const Form = styled.form`
@@ -43,11 +62,12 @@ function LoginPage() {
     height: 64px;
     border-radius: 6px;
     border: 1px solid #ddd;
-    background-color: #fafafa;
-    padding: 20px 10px;
+    background-color: white;
+    padding: 20px 20px;
     box-sizing: border-box;
     display: flex;
     align-items: center;
+    margin-bottom: 5px;
   `;
 
   const Input = styled.input`
@@ -55,7 +75,9 @@ function LoginPage() {
     background: none;
   `;
 
-  const CheckBoxDiv = styled.div``;
+  const CheckBoxDiv = styled.div`
+    margin-bottom: 20px;
+  `;
 
   const CheckBoxLabel = styled.label``;
 
@@ -71,45 +93,76 @@ function LoginPage() {
   `;
 
   const JoinDiv = styled.div`
+    width: 486px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 1px solid #ccc;
+    padding: 30px 15px;
+    box-sizing: border-box;
+    > p {
+      color: #b4b4b4;
+      font-size: 14px;
+      line-height: 19.79px;
+    }
+    > a {
+      font-size: 18px;
+      line-height: 19.78px;
+      color: rgba(209, 30, 26, 1);
+      text-decoration: none;
+      border-bottom: 1px solid rgba(209, 30, 26, 0.5);
+      padding-bottom: 5px;
+    }
   `;
+
+  const SocialLogin = styled.div``;
+
+  const SocialIconBox = styled.div``;
   return (
     <>
       <NavBar>
-        <Logo>와글와글</Logo>
+        <NavLogo>와글와글</NavLogo>
         <FaBars color="white" size={27} />
       </NavBar>
       <Main>
-        <Logo>와글와글</Logo>
-        <Form>
-          <InputDiv>
-            <FaRegUser />
-            <Input type="text" placeholder="호명을 적어주시오." />
-          </InputDiv>
-          <InputDiv>
-            <HiOutlineLockClosed />
-            <Input type="password" placeholder="암호를 적어주시오." />
-          </InputDiv>
-          <CheckBoxDiv>
-            <Input type="checkbox" id="checkbox_input" />
-            <CheckBoxLabel htmlFor="checkbox_input">
-              호명 기억하기
-            </CheckBoxLabel>
-          </CheckBoxDiv>
-          <SubmitButton type="submit">이리 오너라</SubmitButton>
-        </Form>
-        <JoinDiv>
-          <p>
-            자네, 이곳은 처음이오?
-            <br />
-            이곳은 호패가 없으면 들어갈 수가 없다네
-            <br />
-            관청에 가서 호패를 만들 수 있다네
-          </p>
-          <Link to={"/join"}>호패 만들러 가기</Link>
-        </JoinDiv>
+        <MainDiv>
+          <Logo>와글와글</Logo>
+          <Form>
+            <InputDiv>
+              <FaRegUser />
+              <Input type="text" placeholder="호명을 적어주시오." />
+            </InputDiv>
+            <InputDiv>
+              <HiOutlineLockClosed />
+              <Input type="password" placeholder="암호를 적어주시오." />
+            </InputDiv>
+            <CheckBoxDiv>
+              <Input type="checkbox" id="checkbox_input" />
+              <CheckBoxLabel htmlFor="checkbox_input">
+                호명 기억하기
+              </CheckBoxLabel>
+            </CheckBoxDiv>
+            <SubmitButton type="submit">이리 오너라</SubmitButton>
+          </Form>
+          <JoinDiv>
+            <p>
+              자네, 이곳은 처음이오?
+              <br />
+              이곳은 호패가 없으면 들어갈 수가 없다네
+              <br />
+              관청에 가서 호패를 만들 수 있다네
+            </p>
+            <Link to={"/join"}>호패 만들러 가기</Link>
+          </JoinDiv>
+          <SocialLogin>
+            <h4>간편 호패 만들기</h4>
+            <SocialIconBox>
+              <span>카카오</span>
+              <span>구글</span>
+              <span>네이버</span>
+            </SocialIconBox>
+          </SocialLogin>
+        </MainDiv>
       </Main>
     </>
   );
