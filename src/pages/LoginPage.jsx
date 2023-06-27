b6010292ce791360656a5b39d3450a3adeef27fe
YukDDong <ekfhd5537@naver.com>
2023-06-27 14:26:00 +0900

    feat: styled-components
    styled-components 설치 후 적용 중

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,24 @@
+import styled from "styled-components";
+
 function LoginPage() {
+  const NavBar = styled.nav`
+    width: 100%;
+    height: 70px;
+    background-color: rgba(26, 42, 83, 1);
+  `;
+
+  const Logo = styled.h1`
+    width: 180px;
+    height: 50px;
+    background-color: white;
+    font-size: 40px;
+    text-align: center;
+  `;
   return (
     <>
-      <div>LoginPage</div>
+      <NavBar>
+        <Logo>와글와글</Logo>
+      </NavBar>
     </>
   );
 }
