614a0aab2863983c5aeb9afaca445ab510bb6f25
YukDDong <ekfhd5537@naver.com>
2023-07-01 03:30:35 +0900

    Design: Login 페이지 CSS 1차완료
    - Login 페이지 CSS 작업 1차 마무리
    - Component를 최대한 쪼개보려고 기능별로 나눠보려고 했음

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,82 +2,9 @@ import styled from "styled-components";
 import { FaBars } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import LoginForm from "../component/LoginForm/LoginForm";
+import SocialLogin from "../component/socialLogin/SocialLogin";
 
-function LoginPage() {
-  // styled-component CSS작업
-  const NavBar = styled.nav`
-    width: 100%;
-    height: 70px;
-    background-color: rgba(26, 42, 83, 1);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0px 10px;
-    box-sizing: border-box;
-  `;
-
-  const NavLogo = styled.h1`
-    width: 170px;
-    height: 40px;
-    background-color: rgba(139, 139, 139, 0.3);
-    font-size: 35px;
-    text-align: center;
-    line-height: 40px;
-    color: white;
-  `;
-
-  const Main = styled.main`
-    width: 100%;
-    height: auto;
-    display: flex;
-    justify-content: center;
-  `;
-
-  const MainDiv = styled.div`
-    width: 500px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-top: 100px;
-    box-sizing: border-box;
-  `;
-
-  const Logo = styled.h1`
-    width: 170px;
-    height: 40px;
-    background-color: rgba(139, 139, 139, 0.3);
-    font-size: 35px;
-    text-align: center;
-    line-height: 40px;
-    margin-bottom: 30px;
-  `;
-
-  const JoinDiv = styled.div`
-    width: 486px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid #ccc;
-    padding: 30px 15px;
-    box-sizing: border-box;
-    > p {
-      color: #b4b4b4;
-      font-size: 14px;
-      line-height: 19.79px;
-    }
-    > a {
-      font-size: 18px;
-      line-height: 19.78px;
-      color: rgba(209, 30, 26, 1);
-      text-decoration: none;
-      border-bottom: 1px solid rgba(209, 30, 26, 0.5);
-      padding-bottom: 5px;
-    }
-  `;
-
-  const SocialLogin = styled.div``;
-
-  const SocialIconBox = styled.div``;
+export default function LoginPage() {
   return (
     <>
       <NavBar>
@@ -98,18 +25,87 @@ function LoginPage() {
             </p>
             <Link to={"/join"}>호패 만들러 가기</Link>
           </JoinDiv>
-          <SocialLogin>
-            <h4>간편 호패 만들기</h4>
-            <SocialIconBox>
-              <span>카카오</span>
-              <span>구글</span>
-              <span>네이버</span>
-            </SocialIconBox>
-          </SocialLogin>
+          <SocialLoginText>간편 호패 만들기</SocialLoginText>
+          <SocialLogin />
         </MainDiv>
       </Main>
     </>
   );
 }
 
-export default LoginPage;
+// styled-component CSS작업
+const NavBar = styled.nav`
+  width: 100%;
+  height: 70px;
+  background-color: rgba(26, 42, 83, 1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+  box-sizing: border-box;
+`;
+
+const NavLogo = styled.h1`
+  width: 170px;
+  height: 40px;
+  background-color: rgba(139, 139, 139, 0.3);
+  font-size: 35px;
+  text-align: center;
+  line-height: 40px;
+  color: white;
+`;
+
+const Main = styled.main`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+`;
+
+const MainDiv = styled.div`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 100px;
+  box-sizing: border-box;
+`;
+
+const Logo = styled.h1`
+  width: 170px;
+  height: 40px;
+  background-color: rgba(139, 139, 139, 0.3);
+  font-size: 35px;
+  text-align: center;
+  line-height: 40px;
+  margin-bottom: 30px;
+`;
+
+const JoinDiv = styled.div`
+  width: 486px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  padding: 30px 15px;
+  box-sizing: border-box;
+  > p {
+    color: #b4b4b4;
+    font-size: 14px;
+    line-height: 19.79px;
+  }
+  > a {
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 19.78px;
+    color: #e75852;
+    text-decoration: none;
+    border-bottom: 1px solid #e75852;
+    padding-bottom: 5px;
+  }
+`;
+
+const SocialLoginText = styled.h4`
+  margin-top: 30px;
+  font-weight: 600;
+`;
