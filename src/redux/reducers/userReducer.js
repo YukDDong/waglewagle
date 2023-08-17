0e1936af8b9c7490dd189b6fa2a748c3d0b418a6
YukDDong <ekfhd5537@naver.com>
2023-08-17 13:15:56 +0900

    feat: 로그인 파트 리덕스 구현

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,35 @@
+import { LOGIN_USER, LOGOUT_USER, login, logout } from "../actions/userActions";
+
+const initialState = {
+  User: {
+    id: "",
+    name: "",
+    loggedIn: false,
+  },
+};
+
+// user의 로그인 유지와 로그아웃을 위한 Reducer
+export const userCheckReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return {
+        ...state,
+        User: {
+          id: action.payload.id,
+          name: action.payload.name,
+          loggedIn: action.payload.loggedIn,
+        },
+      };
+    case LOGOUT_USER:
+      return {
+        ...state,
+        User: {
+          id: "",
+          name: "",
+          loggedIn: false,
+        },
+      };
+    default:
+      return state;
+  }
+};
