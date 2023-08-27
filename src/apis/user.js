84811e1de0b2aebe07aeee7309ef762ce2addaad
YukDDong <ekfhd5537@naver.com>
2023-08-28 02:28:15 +0900

    feat: 백엔트 api 테스트

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,19 +1,19 @@
 import { client } from "./index";
 
 // 로그인 시 post 요청
-export const login = async (payload) => {
+export const loginApi = async (payload) => {
   const response = await client.post("/authenticate", payload);
   return response;
 };
 
 // 회원가입 시 post 요청
-export const join = async (payload) => {
+export const joinApi = async (payload) => {
   const response = await client.post("/user/signup", payload);
   return response;
 };
 
 // TODO-GOGI : 테스트용으로 추후 삭제 예정
-export const jwtTest = async () => {
+export const jwtTestApi = async () => {
   const response = await client.get("/api/v1/test");
   return response;
 };
