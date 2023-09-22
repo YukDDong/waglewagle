e16e5835077014a780fde9be80de8f720c4814cc
dhgim <hyeonation@gmail.com>
2023-09-22 17:53:05 +0900

    style: 코드 정리. ValidTest 적용. Join.jsx

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import Form from "../../component/Form/Form";
 import NavBar from "../../component/NavBar/NavBar";
 import { styled } from "styled-components";
@@ -8,6 +8,7 @@ import { joinApi } from "../../apis/user";
 import ModalBasic from "../../component/Modal/ModalBasic";
 import { InputText, InputPwd } from "../../component/Input/Input";
 import ButtonActDeact from "../../component/Button/Button";
+import { validEmail, validPwd, IsTrue, IsFalse, CheckInfo } from "../../component/ValidTest/ValidTest";
 
 const Join = () => {
   const [{ userId, password, checkPassword }, setJoinInfo] = useState({
@@ -125,12 +126,23 @@ const JoinRefine = () => {
   });
 
   // 함수
-  const validUserInfo = useCallback(
-    (name, value) => {
-      setIsValid({ ...isValid, [name]: value });
-    },
-    [isValid]
-  );
+  // const updateValid = useCallback(
+  //   (name, value) => {
+  //     setIsValid({ ...isValid, [name]: value });
+  //   },
+  //   [isValid]
+  // );
+
+  // 실시간 변수 업데이트
+  // 변수 업데이트는 렌더링 끝나고 적용되는 듯하다.
+  useEffect(() => {
+    setIsValid({...isValid, 
+      isEmail: validEmail(data.userId),
+      isPassword: validPwd(data.pwd),
+      isPasswordConfirm: (data.pwd === data.confirmPwd)
+    });
+  }, [data]);
+
 
   //// 회원가입
 
@@ -191,6 +203,7 @@ const JoinRefine = () => {
           <Title title="회원가입" />
           <Sub>회원가입에 필요한 정보를 입력해주세요.</Sub>
 
+
           {/* Email */}
           <InputText
             placeholder="이메일을 적어주세요."
@@ -198,18 +211,55 @@ const JoinRefine = () => {
             updateData={updateData}
           />
 
-          {/* Password */}
+          {/* Email 판별  */}
+          {(data.userId !== "") ? (
+              (isValid.isEmail) ? (
+                <IsTrue>유효한 이메일입니다.</IsTrue>
+              ) : (
+                <IsFalse>유효하지 않은 이메일입니다.</IsFalse>
+              )
+            ) : null
+          }
+
+
+          {/* 비밀번호 */}
           <InputPwd 
             placeholder="비밀번호를 적어주세요."
             dataName="pwd"
             updateData={updateData}
           />
 
+          {/* 비밀번호 판별 */}
+          {(data.pwd !== "") ? (
+            (isValid.isPassword) ? (
+              <IsTrue>유효한 비밀번호입니다.</IsTrue>
+            ) : (
+              <IsFalse>유효하지 않은 비밀번호입니다.</IsFalse>
+            )
+          ) : (
+            <CheckInfo>
+              <span>* </span>
+              6~16자, 영문 대.소문자, 숫자, 특수문자 중 2개 이상 사용하세요.
+            </CheckInfo>
+          )}
+
+
+          {/* 비밀번호 확인 */}
           <InputPwd 
             placeholder="비밀번호를 한 번 더 적어주세요."
             dataName="confirmPwd"
             updateData={updateData}
           />
+
+          {/* 비밀번호 확인 판별 */}
+          {(data.confirmPwd !== "") ? (
+            (isValid.isPasswordConfirm) ? (
+              <IsTrue>비밀번호가 일치합니다.</IsTrue>
+            ) : (
+              <IsFalse>비밀번호가 일치하지 않습니다.</IsFalse>
+            )
+          ) : null}
+
 
           {/* 회원가입 버튼 */}
           <ButtonActDeact 
